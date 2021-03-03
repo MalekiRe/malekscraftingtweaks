0c771099d3a78330b392132379b09120fed3109e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 06:46:24 -0800

    first commit!

---
+++ src/main/java/malekscraftingtweaks/malekscraftingtweaks/Malekscraftingtweaks.java
@@ -0,0 +1,10 @@
+package malekscraftingtweaks.malekscraftingtweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Malekscraftingtweaks implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
